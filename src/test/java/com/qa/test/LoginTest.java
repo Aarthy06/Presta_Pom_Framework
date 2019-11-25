7c829c9f2d440da8e12de02930c73b47804f98fb
Pr@vin18 <arthymane27@gmail.com>
2019-11-25 19:35:20 +0530

    added some changes

---
+++ src/test/java/com/qa/test/LoginTest.java
@@ -0,0 +1,78 @@
+package com.qa.test;
+
+import java.util.concurrent.TimeUnit;
+
+import org.apache.log4j.Logger;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Listeners;
+import org.testng.annotations.Test;
+
+import com.qa.pages.BaseClass;
+import com.qa.pages.HomePage;
+import com.qa.pages.loginPage;
+@Listeners(com.qa.listener.ListenerTest.class)	
+public class LoginTest extends BaseClass {
+	loginPage Loginpage;
+	HomePage homepage;
+	 Logger log=Logger.getLogger(LoginTest .class);
+	//property file initialize
+	public LoginTest() {
+		super();
+		}
+	
+	@BeforeMethod
+	public void setup() {
+		initialization();
+		Loginpage=new loginPage();
+	}
+	@Test(priority=1)
+	public void CheckValidLogin() {
+		homepage= Loginpage.login(prop.getProperty("Testusername"), prop.getProperty("TestPassWord"));
+	}
+	
+	@Test(priority=2)
+	public void CheckTitle() {
+		log.info("checkkk titleeeeeeeee");
+		
+		String title=driver.getTitle();
+		Assert.assertEquals(title, "SHOPCART > Administration panel (PrestaShop™)");
+	}
+	@Test(priority=3)
+	public void validatechecklogoimg() {
+		log.info("checkkk logo imgggggg");
+	 boolean expimg=Loginpage.logoimg.isDisplayed();
+	  Assert.assertTrue(expimg);
+	}
+	@Test(priority=4)
+	public void Checkinvalidlogin() {
+		Loginpage.username.sendKeys("fdhgfh");
+		Loginpage.pwd.sendKeys("password");
+		Loginpage.signinbtn.click();
+	    String act=Loginpage.errormsg.getText();
+		Assert.assertEquals(act, "Please enter a valid email address.");
+	}
+	@Test(priority=5)
+	public void Checkpwderrormsg() {
+		Loginpage.username.sendKeys("admin@javaclassesinpune.com");
+		Loginpage.pwd.sendKeys("");
+		Loginpage.signinbtn.click();
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+	    String act=Loginpage.pwderrormsg.getText();
+		Assert.assertEquals(act, "This field is required.");
+	}
+	@Test(priority=6)
+	public void Checkstayloginbtn() {
+		Loginpage.checkstayloginbtn();
+	}
+	
+	
+	@AfterMethod
+	public void teardown() {
+		driver.close();
+	}
+}
---
+++ src/test/java/com/qa/util/TestUtil.java
@@ -0,0 +1,69 @@
+package com.qa.util;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import com.qa.pages.BaseClass;
+
+public class TestUtil extends BaseClass{
+	
+	public static long PAGE_LOAD_TIMEOUT=40;
+	public static long IMPLICIT_WAIT=40;
+	WebDriverWait wait=new WebDriverWait(driver, 20);
+	
+	public void Dropdownbox() {
+		WebElement addruleconcerning=driver.findElement(By.xpath("//*[@id=\"product_rule_type_1\"]"));
+	
+	Select s=new Select(addruleconcerning);
+	s.selectByVisibleText("Products");
+}
+
+	public static String TESTDATA_SHEET_PATH ="C:\\Users\\Lenovo\\Desktop\\Testdata.xlsx";
+
+	static Workbook book;
+	static Sheet sheet;
+	static JavascriptExecutor js;
+
+	
+
+	public static Object[][] getTestData(String sheetName) {
+		FileInputStream file = null;
+		try {
+			file = new FileInputStream(TESTDATA_SHEET_PATH);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		try {
+			book = WorkbookFactory.create(file);
+		} catch (InvalidFormatException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		sheet = book.getSheet(sheetName);
+		Object[][] data = new Object[sheet.getLastRowNum()][sheet.getRow(0).getLastCellNum()];
+		// System.out.println(sheet.getLastRowNum() + "--------" +
+		// sheet.getRow(0).getLastCellNum());
+		for (int i = 0; i < sheet.getLastRowNum(); i++) {
+			for (int k = 0; k < sheet.getRow(0).getLastCellNum(); k++) {
+				data[i][k] = sheet.getRow(i + 1).getCell(k).toString();
+				// System.out.println(data[i][k]);
+			}
+		}
+		return data;
+	}
+ 	
+}
